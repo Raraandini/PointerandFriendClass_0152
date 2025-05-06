07d5c28745a23191309a962e5ba45cd1ccf9f394
Raraandinii <raraandinib@gmail.com>
2025-05-06 18:42:12 +0700

    class PersegiPanjang & luas PP

--- File1.cpp
+++ File1.cpp
@@ -4,4 +4,33 @@ using namespace std;
 class PersegiPanjang
 {
 public: // akses modifier
+ int panjang, lebar;
+
+ public:
+    void input()
+    {
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "Masukkan lebar : ";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+    // Lingkaran ol;
 };
+
+class Lingkaran
+{
+ public:
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan jari-jari : ";
+        cin >> jarijari;
+    }
+
+}
