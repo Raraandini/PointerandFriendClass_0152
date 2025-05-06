3aec5df4ace896fb3230aa6e46fc8b8bd7a2cd7f
Raraandinii <raraandinib@gmail.com>
2025-05-06 19:50:38 +0700

    membuat class pelajar

--- Pointersatu.cpp
+++ Pointersatu.cpp
@@ -3,11 +3,21 @@ using namespace std;
 class mahasiswa{
     public:
     int nim;
-    void showNIM(){
+    void showNim(){
         cout <<"No Induk = "<<nim<<endl;
     }
 };
 
 int main(){
-    
+    mahasiswa mhs{1}; // objek mhs
+    mhs.showNim();
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2;
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs->nim = 3;
+    pMhs->showNim();
+    return 0;
 }
